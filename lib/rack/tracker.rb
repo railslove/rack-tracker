821db2f4cebbc8f9b797d15802388ace2f68b850
Lars Brillert <lars@railslove.com>
2014-08-19 17:35:15 +0200

    inital commit

---
+++ lib/rack/tracker.rb
@@ -0,0 +1,7 @@
+require "rack/tracker/version"
+
+module Rack
+  module Tracker
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/tracker/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Tracker
+    VERSION = "0.0.1"
+  end
+end
