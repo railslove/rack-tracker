f9c300318cb80f579c60e099632f8c301f6d4155
Marco Schaden <ms@donschado.de>
2014-08-26 17:11:20 +0200

    refactor track method to be common across handler classes

--- lib/rack/tracker/google_analytics/google_analytics.rb
+++ lib/rack/tracker/google_analytics/google_analytics.rb
@@ -31,7 +31,7 @@ class Rack::Tracker::GoogleAnalytics < Rack::Tracker::Handler
     Tilt.new( File.join( File.dirname(__FILE__), 'template', 'google_analytics.erb') ).render(self)
   end
 
-  def self.google_analytics(event)
-    { __callee__.to_s => [Event.new(event)] }
+  def self.track(name, event)
+    { name.to_s => [Event.new(event)] }
   end
 end
--- lib/rack/tracker/handler_delegator.rb
+++ lib/rack/tracker/handler_delegator.rb
@@ -2,22 +2,22 @@ class Rack::Tracker::HandlerDelegator
   class << self
     def method_missing(method_name, *args, &block)
       return super unless respond_to?(method_name)
-      handler(method_name).public_send(method_name, *args, &block)
+      handler(method_name).track(method_name, *args, &block)
     end
 
     def respond_to?(method_name, include_private=false)
-      handler(method_name).respond_to?(method_name)
+      handler(method_name).respond_to?(:track)
     end
 
     private
 
     def handler(method_name)
-      _handler = method_name.to_s.classify.pluralize
+      _handler = method_name.to_s.camelize
       ["Rack::Tracker::#{_handler}", _handler].detect do |const|
         begin
           return const.constantize
         rescue NameError
-          false
+          raise ArgumentError, 'No such Handler'
         end
       end
     end
