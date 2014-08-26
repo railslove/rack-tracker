5e53081bb19cf6b9a57e1587c7b828e1e5e6e58d
Marco Schaden <ms@donschado.de>
2014-08-26 14:51:47 +0200

    tracker is not a module

--- lib/rack/tracker/railtie.rb
+++ lib/rack/tracker/railtie.rb
@@ -1,8 +1,10 @@
-module Rack::Tracker
-  class Railtie < ::Rails::Railtie
-    initializer "rack-tracker.configure_controller" do |app|
-      ActiveSupport.on_load :action_controller do
-        include Rack::Tracker::Controller
+module Rack
+  class Tracker
+    class Railtie < ::Rails::Railtie
+      initializer "rack-tracker.configure_controller" do |app|
+        ActiveSupport.on_load :action_controller do
+          include Rack::Tracker::Controller
+        end
       end
     end
   end
