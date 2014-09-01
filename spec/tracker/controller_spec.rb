fa002862609d6c4110365d93f64c32300c80cc4d
Marco Schaden <ms@donschado.de>
2014-09-01 17:45:35 +0200

    no need for pending - we just keep the old behaviour

--- spec/tracker/controller_spec.rb
+++ spec/tracker/controller_spec.rb
@@ -11,29 +11,17 @@ end
 
 RSpec.describe Rack::Tracker::Controller do
   describe '#tracker' do
-    pending
-    # let(:event) { Rack::Tracker::GoogleAnalytics::Send.new(category: 'foo') }
-    # let(:controller) { TestController.new({}) }
+    let(:event) { Rack::Tracker::GoogleAnalytics::Send.new(category: 'foo') }
+    let(:controller) { TestController.new({}) }
 
-    # context 'controller' do
-    #   it 'writes the event into env' do
-    #     expect {
-    #       controller.index
-    #     }.to change {
-    #       controller.env
-    #     }.from({}).to('tracker' => {'google_analytics' => [event]})
-    #   end
-
-    #   it 'returns only the handlers' do
-    #     expect(
-    #       controller.tracker do |t|
-    #         t.google_analytics category: 'foo'
-    #         2.times do
-    #           t.facebook some: 'thing'
-    #         end
-    #       end
-    #     ).to eql([:google_analytics, :facebook])
-    #   end
-    # end
+    context 'controller' do
+      it 'writes the event into env' do
+        expect {
+          controller.index
+        }.to change {
+          controller.env
+        }.from({}).to('tracker' => {'google_analytics' => [event]})
+      end
+    end
   end
 end
