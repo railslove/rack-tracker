7b14372fb0ee747925073ef389996d1066c58eb8
Jakob Hilden <jakobhilden@gmail.com>
2014-09-17 18:43:56 +0200

    create a failing spec for the bug that GA events get somehow dropped

--- spec/integration/google_analytics_integration_spec.rb
+++ spec/integration/google_analytics_integration_spec.rb
@@ -1,4 +1,5 @@
 require 'support/capybara_app_helper'
+require 'pry'
 
 RSpec.describe "Facebook Integration" do
   before do
@@ -11,7 +12,8 @@ RSpec.describe "Facebook Integration" do
   subject { page }
 
   it "embeds the script tag with tracking event from the controller action" do
-    expect(page).to have_content('ga("ecommerce:addItem",{"id":"1234","affiliation":"Acme Clothing","revenue":"11.99","shipping":"5","tax":"1.29"})')
+    expect(page).to have_content('ga("ecommerce:addTransaction",{"id":"1234","affiliation":"Acme Clothing","revenue":"11.99","shipping":"5","tax":"1.29"})')
+    expect(page).to have_content('ga("ecommerce:addItem",{"id":"1234","name":"Fluffy Pink Bunnies","sku":"DD23444","category":"Party Toys","price":"11.99","quantity":"1"})')
     expect(page).to have_content('ga("send",{"hitType":"event","eventCategory":"button","eventAction":"click","eventLabel":"nav-buttons","eventValue":"X"})')
   end
 end
