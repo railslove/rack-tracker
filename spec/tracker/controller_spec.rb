30649c149062394ae6e4b08337548f2bafb8509b
Marco Schaden <ms@donschado.de>
2014-09-18 10:16:41 +0200

    fix wrong env merging of multiple nested events

--- spec/tracker/controller_spec.rb
+++ spec/tracker/controller_spec.rb
@@ -4,6 +4,17 @@ TestController = Struct.new(:env) do
   def index
     tracker do |t|
       t.google_analytics :send, category: 'foo'
+      t.google_analytics :ecommerce, {
+        type: 'addTransaction',
+        some: 'thing'
+      }
+      %w(foo bar).each do |item|
+        t.google_analytics :ecommerce, {
+          type: 'addItem',
+          name: item
+        }
+      end
+      t.facebook :track, { id: '1', value: 1, currency: 'USD' }
     end
   end
 end
@@ -11,7 +22,11 @@ end
 
 RSpec.describe Rack::Tracker::Controller do
   describe '#tracker' do
-    let(:event) { Rack::Tracker::GoogleAnalytics::Send.new(category: 'foo') }
+    let(:send)       { Rack::Tracker::GoogleAnalytics::Send.new(category: 'foo') }
+    let(:trx)        { Rack::Tracker::GoogleAnalytics::Ecommerce.new(type: 'addTransaction', some: 'thing') }
+    let(:item_foo)   { Rack::Tracker::GoogleAnalytics::Ecommerce.new(type: 'addItem', name: 'foo') }
+    let(:item_bar)   { Rack::Tracker::GoogleAnalytics::Ecommerce.new(type: 'addItem', name: 'bar') }
+    let(:fb_event)   { Rack::Tracker::Facebook::Event.new(id: '1', value: 1, currency: 'USD') }
     let(:controller) { TestController.new({}) }
 
     context 'controller' do
@@ -20,7 +35,7 @@ RSpec.describe Rack::Tracker::Controller do
           controller.index
         }.to change {
           controller.env
-        }.from({}).to('tracker' => {'google_analytics' => [event]})
+        }.from({}).to('tracker' => {'google_analytics' => [send, trx, item_foo, item_bar], 'facebook' => [fb_event]})
       end
     end
   end
