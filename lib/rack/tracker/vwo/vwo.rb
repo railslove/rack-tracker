624c9c2a363219a1ddfad2a90a1ae8933bd74259
Salim Hbeiliny <salim@railslove.com>
2015-05-05 11:56:48 +0200

    one more time "fix constant definition to avoid warnings"

--- lib/rack/tracker/vwo/vwo.rb
+++ lib/rack/tracker/vwo/vwo.rb
@@ -1,4 +1,4 @@
-class Vwo <  Rack::Tracker::Handler
+class Rack::Tracker::Vwo <  Rack::Tracker::Handler
   def render
     Tilt.new( File.join( File.dirname(__FILE__), 'template', 'vwo.erb') ).render(self)
   end
