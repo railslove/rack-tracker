f0c081c739726e19593d5bee6743b0a84fc3ed69
Szymon Madeja <smadeja1@gmail.com>
2016-07-11 11:04:50 +0200

    Allow for dynamic Facebook ID
    This allows the custom_audience attribute of the Facebook tracker to be
    a lambda, and therefore dynamic, mimicking the behaviour of the GA
    tracker described in #34.

--- lib/rack/tracker/facebook/facebook.rb
+++ lib/rack/tracker/facebook/facebook.rb
@@ -11,6 +11,14 @@ class Rack::Tracker::Facebook < Rack::Tracker::Handler
     Tilt.new( File.join( File.dirname(__FILE__), 'template/facebook.erb') ).render(self)
   end
 
+  def custom_audience
+    if options[:custom_audience].respond_to?(:call)
+      options[:custom_audience].call(env)
+    else
+      options[:custom_audience]
+    end
+  end
+
   def self.track(name, *event)
     { name.to_s => [event.last.merge('class_name' => 'Event')] }
   end
