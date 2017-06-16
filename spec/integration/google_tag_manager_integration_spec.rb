16b90ba2e933b7e4f26871261c91ad5879cd16c8
bumi <hello@michaelbumann.com>
2017-06-16 16:37:14 +0200

    Google Tag manager: inject dataLayer array only if required

--- spec/integration/google_tag_manager_integration_spec.rb
+++ spec/integration/google_tag_manager_integration_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe "Google Tag Manager Integration" do
     setup_app(action: :google_tag_manager) do |tracker|
       tracker.handler :google_tag_manager, { container: 'GTM-ABCDEF' }
     end
-    visit '/'
   end
 
   subject { page }
 
   it "embeds the script tag with tracking event from the controller action" do
+    visit '/'
     expect(page.find("head")).to have_content 'GTM-ABCDEF'
     expect(page.find("head")).to have_content "dataLayer.push({\"click\":\"X\",\"price\":10}, {\"transactionProducts\":[{\"sku\":\"DD44\",\"name\":\"T-shirt\"},{\"sku\":\"DD66\",\"name\":\"Jeans\"}]});"
     expect(page.find("body")).to have_xpath '//body/noscript/iframe[@src="https://www.googletagmanager.com/ns.html?id=GTM-ABCDEF"]'
   end
 
+  it "does not inject a dataLaye if no events are set " do
+    visit '/?no_events=true'
+    expect(page.find("head")).to have_content 'GTM-ABCDEF'
+    expect(page.find("head")).to_not have_content "dataLayer.push("
+    expect(page.find("body")).to have_xpath '//body/noscript/iframe[@src="https://www.googletagmanager.com/ns.html?id=GTM-ABCDEF"]'
+  end
 end
