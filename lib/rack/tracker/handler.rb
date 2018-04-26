85c671e2885b734b5c3188ae194e3d330f552789
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2018-04-26 20:15:22 +0000

    Fix for frozen response

--- lib/rack/tracker/handler.rb
+++ lib/rack/tracker/handler.rb
@@ -46,10 +46,9 @@ class Rack::Tracker::Handler
     # Sub! is enough, in well formed html there's only one head or body tag.
     # Block syntax need to be used, otherwise backslashes in input will mess the output.
     # @see http://stackoverflow.com/a/4149087/518204 and https://github.com/railslove/rack-tracker/issues/50
-    response.sub! %r{</#{self.position}>} do |m|
+    response.sub %r{</#{self.position}>} do |m|
       self.render << m.to_s
     end
-    response
   end
 
   def write_event(event)
