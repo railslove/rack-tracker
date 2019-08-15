149cfea48aaffd3b667e876e85866f42874e9b80
glaszig <glaszig@gmail.com>
2019-08-16 00:32:38 +0200

    google global: improve integration test

--- spec/integration/google_global_integration_spec.rb
+++ spec/integration/google_global_integration_spec.rb
@@ -3,24 +3,19 @@ require 'support/capybara_app_helper'
 RSpec.describe "Google Global Integration Integration" do
   before do
     setup_app(action: :google_global) do |tracker|
-      tracker.handler :google_global, trackers: [{ id: 'U-XXX-Y' }]
+      tracker.handler :google_global, tracker_options
     end
     visit '/'
   end
 
-  subject { page }
+  let(:tracker_options) { { trackers: [{ id: 'U-XXX-Y' }] } }
 
   it "embeds the script tag with tracking event from the controller action" do
     expect(page.find("head")).to have_content('U-XXX-Y')
   end
 
   describe 'adjust tracker position via options' do
-    before do
-      setup_app(action: :google_global) do |tracker|
-        tracker.handler :google_global, trackers: [{ id: 'U-XXX-Y' }], position: :body
-      end
-      visit '/'
-    end
+    let(:tracker_options) { { trackers: [{ id: 'U-XXX-Y' }], position: :body } }
 
     it "will be placed in the specified tag" do
      expect(page.find("head")).to_not have_content('U-XXX-Y')
@@ -29,15 +24,10 @@ RSpec.describe "Google Global Integration Integration" do
   end
 
   describe "handles empty tracker id" do
-    before do
-      setup_app(action: :google_global) do |tracker|
-        tracker.handler :google_global, trackers: [{ id: nil }, { id: "" }, { id: "  " }]
-      end
-      visit '/'
-    end
+    let(:tracker_options) { { trackers: [{ id: nil }, { id: "" }, { id: "  " }] } }
 
     it "does not inject scripts" do
-      expect(page.find("head")).not_to have_content("<script async src='https://www.googletagmanager.com/gtag/js?id=")
+      expect(page.find("head")).to_not have_content("<script async src='https://www.googletagmanager.com/gtag/js?id=")
     end
   end
 end
