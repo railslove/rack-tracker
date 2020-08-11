379b91a4d562f070ae03e1eef10424f61b0d0590
Ryan Romanchuk <rromanchuk@gmail.com>
2020-08-11 14:02:02 -0700

    support advanced matching in init

--- lib/rack/tracker/facebook_pixel/facebook_pixel.rb
+++ lib/rack/tracker/facebook_pixel/facebook_pixel.rb
@@ -1,6 +1,6 @@
 class Rack::Tracker::FacebookPixel < Rack::Tracker::Handler
   self.position = :body
-  self.allowed_tracker_options = [:id]
+  self.allowed_tracker_options = [:id, :advanced_matching]
 
   class Event < OpenStruct
     def write
